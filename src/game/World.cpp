4e3012bb5799c498f74e3d8de7beaeea39bc1161
Salja <salja8840@gmail.com>
2014-09-15 19:37:49 +0200

    Eluna Sync
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -878,6 +878,9 @@ void World::LoadConfigSettings(bool reload)
     sLog.outString("WORLD: mmap pathfinding %sabled", getConfig(CONFIG_BOOL_MMAP_ENABLED) ? "en" : "dis");
 
     setConfig(CONFIG_BOOL_ELUNA_ENABLED, "Eluna.Enabled", true);
+
+    if (reload)
+        sEluna->OnConfigLoad(reload);
 }
 
 /// Initialize the World
@@ -1293,6 +1296,7 @@ void World::SetInitialWorldSettings()
     ///- Initialize Lua Engine
     sLog.outString("Initialize Eluna Lua Engine...");
     Eluna::Initialize();
+    sEluna->OnConfigLoad(false); // Must be done after Eluna is initialized.
 
     ///- Initialize game time and timers
     sLog.outString("DEBUG:: Initialize game time and timers");
@@ -1540,6 +1544,9 @@ void World::Update(uint32 diff)
 
     // And last, but not least handle the issued cli commands
     ProcessCliCommands();
+
+    ///- Used by Eluna
+    sEluna->OnWorldUpdate(diff);
 
     // cleanup unused GridMap objects as well as VMaps
     sTerrainMgr.Update(diff);
@@ -1835,6 +1842,8 @@ void World::ShutdownServ(uint32 time, uint32 options, uint8 exitcode)
         m_ShutdownTimer = time;
         ShutdownMsg(true);
     }
+    ///- Used by Eluna
+    sEluna->OnShutdownInitiate(ShutdownExitCode(exitcode), ShutdownMask(options));
 }
 
 /// Display a shutdown message to the user(s)
@@ -1876,6 +1885,9 @@ void World::ShutdownCancel()
     SendServerMessage(msgid);
 
     DEBUG_LOG("Server %s cancelled.", (m_ShutdownMask & SHUTDOWN_MASK_RESTART ? "restart" : "shutdown"));
+
+    ///- Used by Eluna
+    sEluna->OnShutdownCancel();
 }
 
 void World::UpdateSessions(uint32 /*diff*/)
